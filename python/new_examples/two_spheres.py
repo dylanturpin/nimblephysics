7b992b41c8ed28698cbd6aafefe312f7833cdedf
Dylan Turpin <9turpin@gmail.com>
2021-05-16 13:58:26 -0400

    normal update through joints

--- python/new_examples/two_spheres.py
+++ python/new_examples/two_spheres.py
@@ -35,6 +35,9 @@ initialState[3:6] = [0.2, 0.0, 0.0]
 initialState[9:12] = [0.0, 0.0, 0.0]
 initialState = torch.tensor(initialState, requires_grad=True)
 
+ikMap = nimble.neural.IKMapping(world)
+ikMap.addLinearBodyNode(sphereA.getBodyNode(0))
+
 state = initialState
 contacts = nimble.contacts(world, state, torch.zeros(world.getActionSize()))
 
@@ -43,40 +46,49 @@ results = []
 results.append(world.getLastCollisionResult())
 for i in range(500):
   print(i)
+  ## UPDATE BASED ON LOSS
   normals = contacts.reshape(-1,6)[:,3:]
   positions = contacts.reshape(-1,6)[:,:3]
   l = -(normals[:,1]-torch.ones_like(normals[:,1])**2).sum()
   l.backward()
 
-  learning_rate = 100.0
-  next_action = torch.zeros(world.getActionSize())
-  next_action[:6] += state.grad[:6]
-  if True:
-    grad_step = state.grad[:6]
-    #pdb.set_trace()
-    #normal_step = normals[0,:]*(state.grad[:6]**2).sum()**(0.5)
-    #next_action[3:] += normal_step
+  with torch.no_grad():
+    learning_rate = 100.0
+    next_action = torch.zeros(world.getActionSize())
+    grad_step = -state.grad[:6] * learning_rate
+    next_action[:6] += grad_step
 
-    next_action *= -learning_rate
     state = state.detach()
-    state[-world.getActionSize():] = 0.
-    state = nimble.timestep(world, state.detach(), next_action)
-    print(state[-world.getActionSize():])
-    state = nimble.timestep(world, state.detach(), torch.zeros(world.getActionSize()))
-    print(state[:world.getActionSize()])
-
-    next_action = torch.zeros(world.getActionSize()) 
-    normal_step = normals[0,:]*(grad_step**2).sum()**(0.5)
-    next_action[3:] += normal_step
-    state = state.detach()
-    next_action *= -learning_rate
     state[-world.getActionSize():] = 0.
     state = nimble.timestep(world, state.detach(), next_action)
     state = nimble.timestep(world, state.detach(), torch.zeros(world.getActionSize()))
 
-    state = torch.tensor(state, requires_grad=True)
-    state.grad = None
-    contacts = nimble.contacts(world, state, torch.zeros(world.getActionSize()))
+  ## UPDATE BASED ON NORMAL
+  state = torch.tensor(state, requires_grad=True)
+  state.grad=None
+  world_pos = nimble.map_to_pos(world, ikMap, state)
+  l = ((world_pos - (world_pos.detach() - normals[0,:].detach()))**2).sum()**(0.5)
+  l.backward()
+  with torch.no_grad():
+    next_action = torch.zeros(world.getActionSize())
+    next_action[:6] += - state.grad[:6] * (grad_step**2).sum()**(0.5)
+    state = state.detach()
+    state[-world.getActionSize():] = 0.
+    state = nimble.timestep(world, state.detach(), next_action)
+    state = nimble.timestep(world, state.detach(), torch.zeros(world.getActionSize()))
+
+  #next_action = torch.zeros(world.getActionSize()) 
+  #normal_step = normals[0,:]*(grad_step**2).sum()**(0.5)
+  #next_action[3:] += normal_step
+  #state = state.detach()
+  #next_action *= -learning_rate
+  #state[-world.getActionSize():] = 0.
+  #state = nimble.timestep(world, state.detach(), next_action)
+  #state = nimble.timestep(world, state.detach(), torch.zeros(world.getActionSize()))
+
+  state = torch.tensor(state, requires_grad=True)
+  state.grad = None
+  contacts = nimble.contacts(world, state, torch.zeros(world.getActionSize()))
 
   states.append(state.detach().clone())
   results.append(world.getLastCollisionResult())
